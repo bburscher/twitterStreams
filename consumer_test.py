50c328c099800456ba4dcc7dab1dc07b3cc86bac
turiphro <turiphro@gmail.com>
2016-11-12 21:34:37 +0100

    Say it!

--- consumer_test.py
+++ consumer_test.py
@@ -1,4 +1,4 @@
-import os
+import os, re, sys
 from confluent_kafka import Producer, Consumer, KafkaError
 
 SERVERS = os.environ.get('SERVERS', 'localhost')
@@ -7,6 +7,7 @@ TOPICS = os.environ.get('TOPICS', 'election')
 # if all should receive all messages, or the same if
 # messages should be load-balanced on all clients
 GROUPID = os.environ.get('GROUPID', 'consumertest')
+SAY_IT = bool(os.environ.get('SAY_IT', ''))
 
 
 if __name__ == '__main__':
@@ -21,6 +22,10 @@ if __name__ == '__main__':
         if not msg.error():
             message = msg.value().decode('utf-8')
             print('[RCV] {}'.format(message))
+            if SAY_IT:
+                cleaned = re.sub('@[a-zA-Z0-9]+', '', message).replace('RT ', '')
+                cleaned = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', cleaned)
+                os.system('say "{}"'.format(cleaned))
         elif msg.error().code() != KafkaError._PARTITION_EOF:
             print('[ERR] {}'.format(msg.error))
             running = False
